5cc515f372f44e6cf49c1e198d7a90d0170d482a
Ziftk <alvaro.cab.lag@gmail.com>
2025-04-16 13:24:32 -0600

    update: remove debug logs

--- Golf Maker/Assets/Controller/Scripts/Mouse/Pencil/Pencil.cs
+++ Golf Maker/Assets/Controller/Scripts/Mouse/Pencil/Pencil.cs
@@ -68,29 +68,20 @@ public class Pencil : MonoBehaviour
     public void OnSwitchPencil(InputAction.CallbackContext context){
         if (context.performed){
             int bindingIndex = context.action.GetBindingIndexForControl(context.control);
-            Debug.Log($"SwitchPencil action performed with binding index: {bindingIndex}");
 
             // Handle the binding index to switch pencil states
             switch (bindingIndex){
                 case 0:
-                    Debug.Log("Switching to PointPencilState");
                     currentState = PointPencilState.GetInstance();
-                    Debug.Log(currentState);
                     break;
                 case 1:
-                    Debug.Log("Switching to LinePencilState");
                     currentState = LinePencilState.GetInstance();
-                    Debug.Log(currentState);
                     break;
                 case 2:
-                    Debug.Log("Switching to SquarePencilState");
                     currentState = SquarePencilState.GetInstance();
-                    Debug.Log(currentState);
                     break;
                 case 3:
-                    Debug.Log("Switching to BucketPencilState");
                     currentState = BucketPencilState.GetInstance();
-                    Debug.Log(currentState);
                     break;
                 default:
                     Debug.LogWarning("Unhandled binding index for SwitchPencil action");
