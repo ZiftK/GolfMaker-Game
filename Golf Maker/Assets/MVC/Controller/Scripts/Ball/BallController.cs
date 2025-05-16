6cd5f3e00dde6c40828989e81ce9173c808a604c
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-16 02:38:38 -0600

    add: BallStates

--- Golf Maker/Assets/MVC/Controller/Scripts/Ball/BallController.cs
+++ Golf Maker/Assets/MVC/Controller/Scripts/Ball/BallController.cs
@@ -8,43 +8,97 @@ using UnityEngine.InputSystem;
     )]
 public class BallController : MonoBehaviour
 {
-    private Vector3 initialPosition;
-    private bool isTracking = false;
+    private bool isHiteable;
 
     [SerializeField]
     private float forceMultiplier = 1f;
     Rigidbody2D rb;
 
+
+    // Ball states
+    private BallState currentState;
+    private IBallLeftClickActor currentLeftClickState;
+    private IBallUpdate currentUpdateState;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentState = BalllIdleState.GetInstance();
+        currentLeftClickState = BalllIdleState.GetInstance();
+        currentUpdateState = null;
+        isHiteable = true; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentUpdateState != null)
+        {
+            currentUpdateState.Update(
+                new BallContext(
+                    Vector3Int.RoundToInt(transform.position),
+                    this,
+                    rb,
+                    forceMultiplier
+                    )
+                );
+        }
     }
 
+    public void SwitchBallState(BallState newState, BallContext context)
+    {
+        currentState.OnExitState(context);
+        currentState = newState;
+        currentState.OnEnterState(context);
+    }
+
+    public void SwitchBallLeftClickState(IBallLeftClickActor newState)
+    {
+        currentLeftClickState = newState;
+    }
+
+    public void SwitchBallUpdateState(IBallUpdate newState)
+    {
+        currentUpdateState = newState;
+    }
 
     public void OnLeftClick(InputAction.CallbackContext context){
+
+
+        if (currentLeftClickState == null)
+        {
+            return;
+        }
+
+        if (!isHiteable) return;
+        
         if (context.started){
-            Debug.Log("Left Click");
-            initialPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            isTracking = true;
+
+            currentLeftClickState.OnLeftClick(
+                new BallContext(
+                    Vector3Int.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition)),
+                    this,
+                    rb,
+                    forceMultiplier
+                    )
+                );
         }
 
         if (context.canceled){
-            Debug.Log("Left UnClick");
-
-            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 direction = Vector3Operations.DirectionXY(currentPosition, initialPosition);
-
-            rb.AddForce(direction*forceMultiplier, ForceMode2D.Impulse);
-
-            isTracking = false;
+            
+            currentLeftClickState.OnLeftUnClick(
+                new BallContext(
+                    Vector3Int.RoundToInt(Camera.main.ScreenToWorldPoint(Input.mousePosition)),
+                    this,
+                    rb,
+                    forceMultiplier
+                    )
+                );
         }
         
     }
+
+    public bool IsHiteable() => isHiteable;
+    public void SetHiteable(bool value) => isHiteable = value; 
 
 }
