5ca13facc0d71e1ecb6c2ab016aceb4ad7ac8d50
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-15 23:41:59 -0600

    add: ball object and initial controller

---
+++ Golf Maker/Assets/MVC/Controller/Scripts/Ball/BallController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[RequireComponent(
+    typeof(Rigidbody2D),
+    typeof(CircleCollider2D),
+    typeof(SpriteRenderer)
+    )]
+public class BallController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
