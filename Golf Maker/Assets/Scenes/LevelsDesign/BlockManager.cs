01004b461ccad39972ca1a4290f590c197b437ac
Yael <rayacova37@gmail.com>
2025-05-19 10:31:11 -0600

    Add new texture assets for blocks: lava, lava_2, line_ball_texture, picos, and water
    - Added lava.png and its metadata for block representation.
    - Added lava_2.png and its metadata for an additional block variant.
    - Introduced line_ball_texture.png and its metadata for ball texture.
    - Added picos.png and its metadata for a new block type.
    - Included water.png and its metadata for water block representation.

--- Golf Maker/Assets/Scenes/LevelsDesign/BlockManager.cs
+++ Golf Maker/Assets/Scenes/LevelsDesign/BlockManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class BlockManager : MonoBehaviour
 {
@@ -17,31 +18,39 @@ public class BlockManager : MonoBehaviour
 
     void Start()
     {
+        if (uIDocument == null)
+        {
+            uIDocument = GetComponent<UIDocument>();
+        }
+
+
         var root = uIDocument.rootVisualElement;
         blocksGrid = root.Q<VisualElement>("blocks-grid");
+        Debug.Log(blocksGrid);
 
-        List<BlockData> blocks = new List<BlockData>
-        {
-            new BlockData { icon = "icon1", name = "Block 1" },
-            new BlockData { icon = "icon2", name = "Block 2" },
-            new BlockData { icon = "icon3", name = "Block 3" }
-        };
+
+
+
+        Sprite[] blocks = Resources.LoadAll<Sprite>("Blocks");
 
         RenderBlocks(blocks);
     }
 
-    public void RenderBlocks(List<BlockData> blocks)
+    public void RenderBlocks(Sprite[] blocks)
     {
         blocksGrid.Clear();
+
 
         foreach (var block in blocks)
         {
             var button = blockbuttonTemplete.CloneTree();
-            var icon = button.Q<Image>("icon");
+            var icon = button.Q<VisualElement>("icon");
 
-            icon.AddToClassList(block.icon);
+            icon.style.backgroundImage = new StyleBackground(block);
+            icon.AddToClassList(block.name);
             button.RegisterCallback<ClickEvent>(_ =>
             {
+
                 Debug.Log("Block clicked: " + block.name);
             });
 
