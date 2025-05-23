63c7bb5aeaed8c650011b32c709ced78609c9aa2
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-23 11:04:37 -0600

    add: implement RepositoryManager with save and reset button functionality

---
+++ Golf Maker/Assets/Scenes/LevelsDesign/RepositoryManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RepositoryManager : MonoBehaviour
+{
+    public UIDocument uIDocument;
+
+    private VisualElement saveButton;
+    private VisualElement resetButton;
+
+    private EditorLevelHandler editorLevelHandler;
+
+
+    public void Awake()
+    {
+        editorLevelHandler = EditorLevelHandler.GetInstance();
+
+        var root = uIDocument.rootVisualElement;
+        
+        saveButton = root.Q<Button>("save-tool");
+        saveButton.RegisterCallback<ClickEvent>(_ =>
+        {
+            Debug.Log("Save button clicked");
+            editorLevelHandler.OnSaveLevel();
+        });
+
+        resetButton = root.Q<Button>("reset-tool");
+        resetButton.RegisterCallback<ClickEvent>(_ =>
+        {
+            Debug.Log("Reset button clicked");
+            editorLevelHandler.OnLoadLevel();
+        });
+    }
+}
