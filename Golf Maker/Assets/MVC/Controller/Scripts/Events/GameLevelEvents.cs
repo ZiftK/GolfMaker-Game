b501e7c6b878cf3c08707ae519cf4aba55db92ee
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-25 21:25:16 -0600

    updateL a event to set initial ball position was added

--- Golf Maker/Assets/MVC/Controller/Scripts/Events/GameLevelEvents.cs
+++ Golf Maker/Assets/MVC/Controller/Scripts/Events/GameLevelEvents.cs
@@ -4,9 +4,17 @@ public class GameLevelEvents
 {
     public delegate void LoadLevel(int levelId);
     public static event LoadLevel OnLoadLevelEvent;
-    
+
     public static void TriggerLoadLevel(int levelId)
     {
         OnLoadLevelEvent?.Invoke(levelId);
+    }
+
+    public delegate void SetVector3(Vector3 position);
+
+    public static event SetVector3 SetBallInitialPositionEvent;
+    public static void TriggerSetBallInitialPosition(Vector3 position)
+    {
+        SetBallInitialPositionEvent?.Invoke(position);
     }
 }
