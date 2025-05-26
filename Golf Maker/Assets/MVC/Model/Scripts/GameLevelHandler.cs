1ee788676bf971f511bfbfec04227c0882bc505b
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-25 20:09:35 -0600

    update: GameLevelHandler now is suscribe to LoadLevel event

--- Golf Maker/Assets/MVC/Model/Scripts/GameLevelHandler.cs
+++ Golf Maker/Assets/MVC/Model/Scripts/GameLevelHandler.cs
@@ -2,18 +2,45 @@ using UnityEngine;
 
 public class GameLevelHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    #region Repository
+    private ILevelRepository levelRepository;
+    #endregion Repository
+
+    public Vector3 initialBallPosition = new Vector3(0, 0, 0);
+
+    
     // Awake is called when the script instance is being loaded
     void Awake()
     {
-        EditorLevelHandler levelEventsHandler = EditorLevelHandler.GetInstance();
-        levelEventsHandler.OnLoadLevel();
+        GameLevelEvents.OnLoadLevelEvent += OnLoadLevel;
         Grid2D.Instance.ActivateVisualGrid(false);
      
+    }
+
+    public void OnLoadLevel(int levelId)
+    {
+        levelRepository = new PrimalLevelRepository();
+        LevelEntity level = levelRepository.LoadLevelRecord(levelId);
+
+        if (level == null)
+        {
+            Debug.LogError($"Level with ID {levelId} not found.");
+            return;
+        }
+
+        // set level dimensions
+        Grid2D.Instance.SetLevelHeight(level.levelHeight);
+        Grid2D.Instance.SetLevelWidth(level.levelWidth);
+
+        // deserialize the level structure
+        int[,] levelStructure = LevelParser.DeSerializeLevelIds(level.LevelStructure);
+
+        // Load the level structure into the grid
+        Grid2D.Instance.LoadLevelFromParseLevel(levelStructure);
+
+        // Activate the visual grid
+        Grid2D.Instance.ActivateVisualGrid(false);
     }
 
     // Update is called once per frame
