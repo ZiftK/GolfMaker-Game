310fdd9fae611aa90d8081f9bc544df778dddf10
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-26 10:24:58 -0600

    update: Add TileSetType enum and integrate into TileMapConfig for better tile management

--- Golf Maker/Assets/MVC/Model/ScriptableObjects/TileMapStorage.cs
+++ Golf Maker/Assets/MVC/Model/ScriptableObjects/TileMapStorage.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -7,6 +8,13 @@ public enum TileType
 {
     Solid,
     Background
+}
+
+[Serializable]
+public enum TileSetType
+{
+    Multiple,
+    Single
 }
 
 [Serializable]
@@ -24,11 +32,15 @@ public struct TileMapConfig
 
     public int sortingLayer;
 
+    public TileSetType tileSetType;
+
     [Header("If is a solid block")]
     public PhysicsMaterial2D physicsMaterial;
 
     [Header("If is a background block")]
     public float viscosity;
+
+    
     
     
 }
@@ -36,4 +48,43 @@ public struct TileMapConfig
 public class TileMapStorage : ScriptableObject
 {
     public TileMapConfig[] tileMapComponents;
+}
+
+public static class TileMapStorageConversions
+{
+    public static ITileHandler GetTileHandler(TileMapConfig config, GameObject tileMapObject)
+    {
+        TileSetType tileSetType = config.tileSetType;
+
+        switch (tileSetType)
+        {
+            case TileSetType.Multiple:
+                return tileMapObject.GetComponent<MultipleTileMapHandler>();
+
+            case TileSetType.Single:
+                return tileMapObject.GetComponent<SingleTileMapHandler>();
+
+            default:
+                throw new Exception($"TileBase {config.name} has an invalid tile set type");
+        }
+    }
+
+    public static void AddTileHandler(ref TileMapComponent tileMapComponent)
+    {
+        TileSetType tileSetType = tileMapComponent.config.tileSetType;
+
+        switch (tileSetType)
+        {
+            case TileSetType.Multiple:
+                tileMapComponent.obj.AddComponent<MultipleTileMapHandler>();
+                break;
+
+            case TileSetType.Single:
+                tileMapComponent.obj.AddComponent<SingleTileMapHandler>();
+                break;
+
+            default:
+                throw new Exception($"TileBase {tileMapComponent.config.name} has an invalid tile set type");
+        }
+    }
 }
