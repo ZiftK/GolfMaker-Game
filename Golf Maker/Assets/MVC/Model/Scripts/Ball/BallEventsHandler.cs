9d06e075ea465df68289fb2f65e811c0cab7dbd4
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-25 21:26:18 -0600

    add: BallEventsHandler

---
+++ Golf Maker/Assets/MVC/Model/Scripts/Ball/BallEventsHandler.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BallEventsHandler : MonoBehaviour
+{
+    void Awake()
+    {
+        GameLevelEvents.SetBallInitialPositionEvent += SetPosition;
+    }
+
+    void SetPosition(Vector3 position)
+    {
+        position.x += 0.5f; // Adjusting position to center the ball on the tile
+        position.y += 0.5f; // Adjusting position to center the ball on the tile
+        gameObject.transform.position = position;
+    }
+}
