238edf04092ffb320d5b4f2b7654a42fbd374784
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-25 18:31:52 -0600

    add: implement EffectsEvents class for managing visual effects and event handling

---
+++ Golf Maker/Assets/MVC/Controller/Scripts/Events/EffectEvents.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EffectsEvents
+{
+
+    public delegate void EffectByNameAndObject(string effectName, GameObject effect);
+
+    public static event EffectByNameAndObject EffectDisabled;
+    public static void OnEffectDisabled(string effectName, GameObject effect)
+    {
+        EffectDisabled?.Invoke(effectName, effect);
+    }
+
+    public static void ThrowEffect(string effectName, Vector3 position)
+    {
+        GameObject effect = EffectsFactory.Instance.GetEffect(effectName);
+        effect.transform.position = position;
+        effect.SetActive(true);
+        effect.GetComponent<EffectToPool>().effect.Play();
+    }
+}
