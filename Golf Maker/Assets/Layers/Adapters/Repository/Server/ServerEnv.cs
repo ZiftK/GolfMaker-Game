0e162796630582c873dc99f0ba9e427cafd68d7c
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-28 10:24:39 -0600

    fix: server url's

--- Golf Maker/Assets/Layers/Adapters/Repository/Server/ServerEnv.cs
+++ Golf Maker/Assets/Layers/Adapters/Repository/Server/ServerEnv.cs
@@ -1,7 +1,7 @@
 public class ServerEnv
 {
-    public static string levelsServerUrl = "http://localhost:3000/api/niveles";
-    public static string usersServerUrl = "http://localhost:3000/api/usuarios";
-    public static string ratingsServerUrl = "http://localhost:3000/api/ratings";
-    public static string stadisticsServerUrl = "http://localhost:3000/api/estadisticas";
+    public static string levelsServerUrl = "http://localhost:3000/niveles";
+    public static string usersServerUrl = "http://localhost:3000/usuarios";
+    public static string ratingsServerUrl = "http://localhost:3000/ratings";
+    public static string stadisticsServerUrl = "http://localhost:3000/estadisticas";
 }
