e19d3b4dfc00c87a7b9ef3320e8a9f4b05386487
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-28 09:49:06 -0600

    add: IUserRepository was added

---
+++ Golf Maker/Assets/Layers/Adapters/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public interface IUserRepository
+{
+    Task<List<UserEntity>> GetAll();
+    Task<UserEntity> Create(UserEntity user);
+    Task<UserEntity> Update(int id, UserEntity user);
+    Task Delete(int id);
+    Task<UserEntity> GetById(int id);
+} 
