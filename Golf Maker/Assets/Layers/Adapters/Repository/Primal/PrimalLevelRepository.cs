be1aff0dddc74d04df7485aff2ce624b024996a5
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-28 09:03:29 -0600

    update: ILevelRepository now follows the ts NivelRepository

--- Golf Maker/Assets/Layers/Adapters/Repository/Primal/PrimalLevelRepository.cs
+++ Golf Maker/Assets/Layers/Adapters/Repository/Primal/PrimalLevelRepository.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 public class PrimalLevelRepository : ILevelRepository
@@ -5,12 +7,37 @@ public class PrimalLevelRepository : ILevelRepository
     private const string LevelDataFilePath = "Assets/Resources/LevelData.json";
     private const string LevelDataBackupFilePath = "Assets/Resources/LevelDataBackup.json";
 
+    public Task<LevelEntity> Create(LevelEntity level)
+    {
+        throw new System.NotImplementedException();
+    }
+
     public int CreateLevelRecord(LevelEntity level)
     {
         // todo: Implement the logic to create a level record using file system
         string json = JsonConvert.SerializeObject(level);
         System.IO.File.WriteAllText(LevelDataFilePath, json);
         return 1;
+    }
+
+    public Task Delete(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<List<LevelEntity>> GetAll()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<LevelEntity> GetById(int id)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<List<LevelEntity>> GetByUserId(int userId)
+    {
+        throw new System.NotImplementedException();
     }
 
     public LevelEntity LoadLevelRecord(int id)
@@ -24,5 +51,10 @@ public class PrimalLevelRepository : ILevelRepository
         
 
         return level;
+    }
+
+    public Task<LevelEntity> Update(int id, LevelEntity level)
+    {
+        throw new System.NotImplementedException();
     }
 }
