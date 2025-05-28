5462a0c25d912adb1c8d52b29939bba580c6437f
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-28 09:48:00 -0600

    add: server enviroment variables class was added

---
+++ Golf Maker/Assets/Layers/Adapters/Repository/Server/ServerEnv.cs
@@ -0,0 +1,7 @@
+public class ServerEnv
+{
+    public static string levelsServerUrl = "http://localhost:3000/api/niveles";
+    public static string usersServerUrl = "http://localhost:3000/api/usuarios";
+    public static string ratingsServerUrl = "http://localhost:3000/api/ratings";
+    public static string stadisticsServerUrl = "http://localhost:3000/api/estadisticas";
+}
