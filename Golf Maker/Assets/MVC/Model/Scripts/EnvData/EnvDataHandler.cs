bbc9bace051bf292ceedf874e25ffa9df7e1e0e9
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-28 10:39:04 -0600

    update: EventDataHandler now storage login user data

--- Golf Maker/Assets/MVC/Model/Scripts/EnvData/EnvDataHandler.cs
+++ Golf Maker/Assets/MVC/Model/Scripts/EnvData/EnvDataHandler.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
+[DefaultExecutionOrder(-300)]
 public class EnvDataHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private UserEntity userData;
+    public static EnvDataHandler Instance;
+    void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // Keep this instance across scenes
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy duplicate instances
+        }
+        DontDestroyOnLoad(this.gameObject); // Ensure this object persists across scene loads
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetUserData(UserEntity user)
     {
-        
+        userData = user;
     }
 }
