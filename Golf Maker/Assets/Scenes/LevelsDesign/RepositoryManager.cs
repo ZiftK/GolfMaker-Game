84ccdefe848df8fda924794751584f71d032a2e0
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-28 19:28:00 -0600

    update: SaveLevel function was added

--- Golf Maker/Assets/Scenes/LevelsDesign/RepositoryManager.cs
+++ Golf Maker/Assets/Scenes/LevelsDesign/RepositoryManager.cs
@@ -13,23 +13,24 @@ public class RepositoryManager : MonoBehaviour
 
     private EditorLevelEvents editorLevelEvents;
 
+    private VisualElement root;
 
     public void Awake()
     {
         editorLevelEvents = EditorLevelEvents.GetInstance();
 
-        var root = uIDocument.rootVisualElement;
+        root = uIDocument.rootVisualElement;
 
         saveButton = root.Q<Button>("save-tool");
         saveButton.RegisterCallback<ClickEvent>(_ =>
         {
-            editorLevelEvents.OnSaveLevel();
+            SaveLevel();
         });
 
         resetButton = root.Q<Button>("reset-tool");
         resetButton.RegisterCallback<ClickEvent>(_ =>
         {
-            editorLevelEvents.OnLoadLevel();
+
         });
 
         leaveButton = root.Q<Button>("leave-tool");
@@ -37,5 +38,41 @@ public class RepositoryManager : MonoBehaviour
         {
             SceneManager.LoadScene("Root");
         });
+    }
+
+    void SaveLevel()
+    {
+        string levelName = root.Q<TextField>().value;
+
+        if (string.IsNullOrWhiteSpace(levelName))
+        {
+            Debug.LogError("Level name empty");
+            return;
+        }
+
+        LevelEntity levelData = new LevelEntity
+        {
+            IdNivel = -1,
+            IdUsuario = EnvDataHandler.Instance.GetCurrentUserId(), // Example user ID
+            Nombre = levelName,
+            FechaCreacion = System.DateTime.Now,
+            Dificultad = Dificultad.Medio,
+            Descripcion = "A challenging level with obstacles.",
+            RatingPromedio = 0f,
+            JugadoVeces = 0,
+            CompletadoVeces = 0,
+            CantidadMoneas = 0,
+            AltoNivel = Grid2D.Instance.GetLevelHeight(),
+            AnchoNivel = Grid2D.Instance.GetLevelWidth(),
+            EstructuraNivel = LevelParser.SerializeLevelIds(Grid2D.Instance.GetLevelIds()),
+        };
+
+        editorLevelEvents.OnSaveLevel(levelData);
+    }
+
+    void LoadLevel()
+    {
+
+        editorLevelEvents.OnLoadLevel();
     }
 }
