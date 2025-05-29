34e63a54bba4e9fd9b9cd0410d4c712721f60933
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-28 18:43:41 -0600

    update: events to handle all canvas

--- Golf Maker/Assets/MVC/Model/Scripts/Login/PrimalLoginHandler.cs
+++ Golf Maker/Assets/MVC/Model/Scripts/Login/PrimalLoginHandler.cs
@@ -1,23 +1,82 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PrimalLoginHandler : MonoBehaviour
 {
 
+    [Header("Login form")]
+    public Canvas loginCanvas;
     public TMP_InputField usernameInputField;
     public TMP_InputField passwordInputField;
 
-    public void OnButtonClickEvent()
+    [Header("Main form")]
+    public Canvas mainCanvas;
+
+    [Header("Level list canvas")]
+    public Canvas levelList;
+    public GameObject content;
+    public GameObject buttonPrefab;
+
+    public void OnLoginButtonClickEvent()
     {
         _ = TryLogin();
     }
 
+    public void OnCreateLevelButtonClickEvent()
+    {
+        SceneManager.LoadScene("LevelCreator");
+    }
+
+
+    public void OnLevelListButtonClickEvent()
+    {
+        SwitchCambas(2);
+        foreach (Transform child in content.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        _ = ShowLevelsList();
+    }
+
+    public void OnReturnFromLevelListClickEvent()
+    {
+        SwitchCambas(1);
+    }
+
+    public void SwitchCambas(int id)
+    {
+        mainCanvas.gameObject.SetActive(false);
+        loginCanvas.gameObject.SetActive(false);
+        levelList.gameObject.SetActive(false);
+
+        switch (id)
+        {
+            case 0:
+
+                loginCanvas.gameObject.SetActive(true);
+                break;
+            case 1:
+                mainCanvas.gameObject.SetActive(true);
+                break;
+
+            case 2:
+                levelList.gameObject.SetActive(true);
+                break;
+            default:
+                break;
+
+        }
+    }
+
     public async Task TryLogin()
     {
-        
+
         string username = usernameInputField.text;
         string password = passwordInputField.text;
 
@@ -43,10 +102,29 @@ public class PrimalLoginHandler : MonoBehaviour
         }
 
         EnvDataHandler.Instance.SetUserData(user);
-        SceneManager.LoadScene("LevelCreator");
-
         // Reset input fields after submission
         usernameInputField.text = string.Empty;
         passwordInputField.text = string.Empty;
+        SwitchCambas(1);
+
     }
+
+    public async Task ShowLevelsList()
+    {
+        ILevelRepository levelRepository = ServerLevelRepository.GetInstance();
+        List<LevelEntity> levels = await levelRepository.GetAll();
+
+        if (levels.Count == 0)
+        {
+            Debug.Log("Sin niveles");
+            return;
+        }
+
+        foreach (LevelEntity level in levels)
+        {
+            GameObject button = Instantiate(buttonPrefab, content.transform);
+            button.GetComponent<LevelButton>().Customize(level);
+        }
+    }
+
 }
