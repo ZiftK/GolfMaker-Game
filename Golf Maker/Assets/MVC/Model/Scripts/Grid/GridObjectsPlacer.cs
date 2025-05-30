29e1248cba5d8b06134d5185afaea220dc60332c
Ziftk <alvaro.cab.lag@gmail.com>
2025-05-30 00:45:13 -0600

    update: coins works

--- Golf Maker/Assets/MVC/Model/Scripts/Grid/GridObjectsPlacer.cs
+++ Golf Maker/Assets/MVC/Model/Scripts/Grid/GridObjectsPlacer.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 
@@ -14,32 +15,49 @@ public class GridObjectsPlacer : MonoBehaviour
 {
     List<PlaceObject> objectsInGrid;
     Stack<GameObject> instantiateObjects;
+    HashSet<Vector3Int> positions;
 
     PlaceObjectsFactory placeObjectsFactory;
+
+    float tileBaseWidth;
 
     void Awake()
     {
         placeObjectsFactory = gameObject.GetComponent<PlaceObjectsFactory>();
     }
-    public void InitGridObjectsPlacer()
+    public void InitGridObjectsPlacer(float tileBaseWidth)
     {
         objectsInGrid = new List<PlaceObject>();
+        positions = new HashSet<Vector3Int>();
+        instantiateObjects = new Stack<GameObject>();
+
+        this.tileBaseWidth = tileBaseWidth;
+
         // string json = JsonConvert.SerializeObject(objectInGrid);
     }
 
     public void PlaceObjectAtPosition(Vector3Int position, string placeObjectName)
     {
+        if (positions.Contains(position)) return;
+
         GameObject prefab = PlaceObjectsFactory.GetPlaceObjectByName(placeObjectName);
         GameObject instance = Instantiate(prefab);
 
-        instance.transform.position = position;
+        instance.transform.position = position + new Vector3(tileBaseWidth, tileBaseWidth, 0);
+
+        instantiateObjects.Push(instance);
+        positions.Add(position);
     }
     public void PlaceObjectAtPosition(Vector3Int position, int id)
     {
+        if (positions.Contains(position)) return;
+
         GameObject prefab = PlaceObjectsFactory.GetPlaceObjectById(id);
         GameObject instance = Instantiate(prefab);
 
-        instance.transform.position = position;
+        instance.transform.position = position + new Vector3(tileBaseWidth, tileBaseWidth, 0);
+        instantiateObjects.Push(instance);
+        positions.Add(position);
     }
 
     public string GetParsedStructure() => LevelParser.SerializeLevelObjects(objectsInGrid);
